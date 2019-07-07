0d1ae43d64cfb08eb791497792da4c964626cfe2
Jon Mockbee <mockbee1000@gmail.com>
2019-07-07 18:22:32 -0400

    last changes

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -3,29 +3,76 @@ package com.JonMockbee;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("your final score was" + highScore);
 
 
+        gameOver = true;
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
 
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("your final score was" + highScore);
 
-        calculateScore (true,800,5,100);
-
-        calculateScore (true,10000,8,200);
-
-
-
+        int highScorePosition = calculateHighScorePosition (1500);
+        displayHighScorePosition("Tome", highScorePosition);
+         highScorePosition = calculateHighScorePosition (900);
+        displayHighScorePosition("Bob", highScorePosition);
+         highScorePosition = calculateHighScorePosition (400);
+        displayHighScorePosition("Larry", highScorePosition);
+         highScorePosition = calculateHighScorePosition (50);
+        displayHighScorePosition("Me", highScorePosition);
 
     }
 
-    public static void  calculateScore(boolean gameover, int score, int levelCompleted,int bonus ){
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition +" on the high score table")
+        ;
+    }
+    public static int calculateHighScorePosition (int playerScore){
+//        if (playerScore >=1000){
+//            return 1;
+//        } else if (playerScore >=500) {
+//            return 2;
+//        } else if (playerScore >=100) {
+//            return 3;
+//        } else {
+//            return 4;
+
+        int position = 4; //assuming  the 4 position will be returned
+        if (playerScore >=1000) {
+            position = 1;
+      } else if (playerScore >=500) {
+            position = 2;
+        } else if (playerScore >=100) {
+            position =3 ;
+        }
+        return position;
+    }
+
+
+    public static int calculateScore(boolean gameover, int score, int levelCompleted, int bonus) {
+
 
 
         if (gameover) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was" + finalScore);
+            return finalScore;
         }
+        return -1;
+        // return works like an else
+
+
 
     }
-    }
+}
 
